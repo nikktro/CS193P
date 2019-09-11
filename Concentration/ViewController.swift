ffc7e6fa77706cdd6caf79b622959280c3e00076
Nikolay Trofimov <nikktro@gmail.com>
2019-09-11 13:57:09 +0300

    working 1 card

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
+  @IBAction func touchCard(_ sender: UIButton) {
+    flipCard(with: "👻", on: sender)
+  }
+  
+  
+  func flipCard(with emoji: String, on button: UIButton) {
+    if button.currentTitle == emoji {
+      button.setTitle("", for: .normal)
+      button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+    } else {
+      button.setTitle(emoji, for: .normal)
+      button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    }
   }
 
 
