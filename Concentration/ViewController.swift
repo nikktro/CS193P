8ccb680e031cfc7cdc7ec15525134480244f0df3
Nikolay Trofimov <nikktro@gmail.com>
2019-09-11 12:10:00 +0300

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Nikolay.Trofimov on 11/09/2019.
+//  Copyright © 2019 Nikolay.Trofimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
