a747b5d93f764116cd1172f4747e4cdb2b76266c
Nikolay Trofimov <nikktro@gmail.com>
2019-09-12 15:04:32 +0300

    Create full MVC

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -22,26 +22,41 @@ class ViewController: UIViewController {
   
   @IBOutlet var cardButtons: [UIButton]!
   
-  var emojiChoices = ["👻","🎃","👻","🎃"]
-  
   @IBAction func touchCard(_ sender: UIButton) {
     flipCount += 1
     if let cardNumber = cardButtons.firstIndex(of: sender) {
-      flipCard(with: emojiChoices[cardNumber], on: sender)
-      print("CardNumber = \(cardNumber)")
+      game.chooseCard(at: cardNumber)
+      updateViewFromModel()
     } else {
       print("Card is not in array CardButtons")
     }
   }
-
-  func flipCard(with emoji: String, on button: UIButton) {
-    print("flipCard with emoji: \(emoji)")
-    if button.currentTitle == emoji {
-      button.setTitle("", for: .normal)
-      button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
-    } else {
-      button.setTitle(emoji, for: .normal)
-      button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+  
+  func updateViewFromModel() {
+    for index in 0..<cardButtons.count {
+      let button = cardButtons[index]
+      let card = game.cards[index]
+      if card.isFaceUp {
+        button.setTitle(emoji(for: card), for: .normal)
+        button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+      } else {
+        button.setTitle("", for: .normal)
+        button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+      }
     }
   }
+  
+  //var emojiChoices = ["👻","🎃","👻","🎃"]
+  var emojiChoices = ["👻","🎃","😈","🍂","🍬","🍭","😱","🧙‍♀️","🧟‍♂️"]
+  
+  var emoji = [Int:String]()
+  
+  func emoji(for card: Card) -> String {
+    if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+      let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+      emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+    }
+    return emoji[card.identifier] ?? "?"
+  }
+
 }
