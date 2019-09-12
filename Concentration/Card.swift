fbc2021f548ac921495311845ae647195c6e5ee0
Nikolay Trofimov <nikktro@gmail.com>
2019-09-12 12:07:33 +0300

    add model files and add basic code to it

---
+++ Concentration/Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Nikolay.Trofimov on 12/09/2019.
+//  Copyright © 2019 Nikolay.Trofimov. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+  
+  var isFaceUp = false
+  var isMatched = false
+  var identifier: Int
+  
+}
---
+++ Concentration/Concentration.swift
@@ -0,0 +1,19 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Nikolay.Trofimov on 12/09/2019.
+//  Copyright © 2019 Nikolay.Trofimov. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+  
+  var cards = [Card]()
+  
+  func chooseCard(at index: Int) {
+    
+  }
+  
+}
